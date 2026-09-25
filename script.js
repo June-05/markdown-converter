fc75dd4c16a7ccabe121d2d241faa0b1a1d1b3cb
Jun Mari Manla <jmanla_250000000236@uic.edu.ph>
2026-09-25 18:28:27 +0800

    added browser-like tab functionality.

--- script.js
+++ script.js
@@ -1,56 +1,141 @@
 const patterns = [
-  {pattern: /^### (.*$)/gim,
-  result: "<h3>$1</h3>"},
-  {
-    pattern: /^## (.*$)/gim,
-    result: "<h2>$1</h2>"
-  },
-  {
-    pattern: /^# (.*$)/gim,
-    result: "<h1>$1</h1>"
-  },
-  {
-    pattern: /^>\s?(.*)$/gim,
-    result: "<blockquote>$1</blockquote>"
-  },
-  {
-    pattern: /(\*\*|__)(.*?)\1/g,
-    result: "<strong>$2</strong>"
-  },
-  {
-    pattern: /(\*|_)(.*?)\1/g, 
-    result: "<em>$2</em>"
-  },
-  {
-    pattern: /!\[([^\]]+)\]\(([^)]+)\)/g,
-    result: `<img alt="$1" src="$2">`
-  },
-  {
-    pattern: /(?<!!)\[([^\]]+)\]\(([^)]+)\)/g,
-    result: `<a href="$2">$1</a>`
-  }];
-
-
+    { pattern: /^### (.*$)/gim, result: "<h3>$1</h3>" },
+    {
+        pattern: /^## (.*$)/gim,
+        result: "<h2>$1</h2>",
+    },
+    {
+        pattern: /^# (.*$)/gim,
+        result: "<h1>$1</h1>",
+    },
+    {
+        pattern: /^>\s?(.*)$/gim,
+        result: "<blockquote>$1</blockquote>",
+    },
+    {
+        pattern: /(\*\*|__)(.*?)\1/g,
+        result: "<strong>$2</strong>",
+    },
+    {
+        pattern: /(\*|_)(.*?)\1/g,
+        result: "<em>$2</em>",
+    },
+    {
+        pattern: /!\[([^\]]+)\]\(([^)]+)\)/g,
+        result: `<img alt="$1" src="$2">`,
+    },
+    {
+        pattern: /(?<!!)\[([^\]]+)\]\(([^)]+)\)/g,
+        result: `<a href="$2">$1</a>`,
+    },
+];
+//FOR Markdown to HTML conversion
 const input = document.getElementById("markdown-input");
 const htmlOutput = document.getElementById("html-output");
 const preview = document.getElementById("preview");
 
 input.addEventListener("input", handleConversion);
 
-function handleConversion(){
-  let result = convertMarkdown();
+function handleConversion() {
+    let result = convertMarkdown();
 
-  htmlOutput.textContent = result;
-  preview.innerHTML = result;
+    htmlOutput.textContent = result;
+    preview.innerHTML = result;
 }
 
+function convertMarkdown() {
+    let markDown = input.value;
 
-function convertMarkdown(){
-  let markDown = input.value;
-  
-  //convert here
-  for(let obj of patterns){
-    markDown = markDown.replace(obj.pattern,obj.result);
-  }
-  return markDown;
+    //convert here
+    for (let obj of patterns) {
+        markDown = markDown.replace(obj.pattern, obj.result);
+    }
+    return markDown;
 }
+
+//FOR Tab Behavior
+const inputContainer = document.getElementById("inputContainer");
+const htmlContainer = document.getElementById("htmlContainer");
+const previewContainer = document.getElementById("previewContainer");
+
+const inputTab = document.getElementById("inputTab");
+const htmlTab = document.getElementById("htmlTab");
+const previewTab = document.getElementById("previewTab");
+
+//initial state
+inputContainer.style.display = "block";
+inputTab.classList.add("active");
+
+//switching between tabs
+inputTab.addEventListener("click", () => {
+    inputContainer.style.display = "block";
+    htmlContainer.style.display = "none";
+    previewContainer.style.display = "none";
+
+    inputTab.classList.add("active");
+    htmlTab.classList.remove("active");
+    previewTab.classList.remove("active");
+});
+
+htmlTab.addEventListener("click", () => {
+    inputContainer.style.display = "none";
+    htmlContainer.style.display = "block";
+    previewContainer.style.display = "none";
+
+    inputTab.classList.remove("active");
+    htmlTab.classList.add("active");
+    previewTab.classList.remove("active");
+});
+
+previewTab.addEventListener("click", () => {
+    inputContainer.style.display = "none";
+    htmlContainer.style.display = "none";
+    previewContainer.style.display = "block";
+
+    inputTab.classList.remove("active");
+    htmlTab.classList.remove("active");
+    previewTab.classList.add("active");
+});
+
+//maximizing tabs
+const inputMax = document.getElementById("inputMax");
+const htmlMax = document.getElementById("htmlMax");
+const previewMax = document.getElementById("previewMax");
+
+//inital state
+inputContainer.classList.add("maximized");
+inputMax.textContent = "Min";
+
+inputMax.addEventListener("click", (event) => {
+    const isBeingMaximized = inputContainer.classList.toggle("maximized");
+    if(isBeingMaximized) {
+      inputMax.textContent = "Min";
+    } else {
+      inputMax.textContent = "Max";
+    }
+
+    event.stopPropagation();
+});
+
+htmlMax.addEventListener("click", (event) => {
+    const isBeingMaximized = htmlContainer.classList.toggle("maximized");
+    if(isBeingMaximized) {
+      htmlMax.textContent = "Min";
+    } else {
+      htmlMax.textContent = "Max";
+    }
+
+    event.stopPropagation();
+
+});
+
+previewMax.addEventListener("click", (event) => {
+    const isBeingMaximized = previewContainer.classList.toggle("maximized");
+    if(isBeingMaximized) {
+      previewMax.textContent = "Min";
+    } else {
+      previewMax.textContent = "Max";
+    }
+
+    event.stopPropagation();
+});
